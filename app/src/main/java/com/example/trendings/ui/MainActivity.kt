225780a5d6c60560fbfdcc272fccd03950df0b08
Malik Dawar <malikdawar@hotmail.com>
2021-07-31 23:11:39 +0200

    day and night mode

--- app/src/main/java/com/example/trendings/ui/MainActivity.kt
+++ app/src/main/java/com/example/trendings/ui/MainActivity.kt
@@ -33,4 +33,29 @@ class MainActivity : BaseActivity() {
         replaceFragmentSafely(TrendingFragment())
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_day_night_mode -> {
+                // Get new mode.
+                val mode =
+                    if ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
+                        Configuration.UI_MODE_NIGHT_NO
+                    ) {
+                        AppCompatDelegate.MODE_NIGHT_YES
+                    } else {
+                        AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                    }
+
+                // Change UI Mode
+                AppCompatDelegate.setDefaultNightMode(mode)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
